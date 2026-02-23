f739bbf4e867ddfa6457eadc0bc2f5da77687a04
jinx487 <kinyanjuimaureen091@gmail.com>
2026-02-23 10:01:49 +0300

    login and signup simulation( built from memory)

---
+++ login_signup_simulation.py
@@ -0,0 +1,12 @@
+print('Create an account')
+username = input('Enter a username: ')
+password = input('Enter a password: ')
+
+print('You have successfully created an account')
+print('Login now!')
+
+username2 = input('Enter a username: ')
+password2 = input('Enter a password: ')
+
+if username == username2 and password == password2:
+    print('Logged in successfully')
